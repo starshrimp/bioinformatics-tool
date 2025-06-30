737d02c1208ccc2deea96650f979ea058b35194b
Sarah <sarah.meyer@students.fhnw.ch>
2025-06-30 14:28:42 +0200

    sidebar nicer

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemText } from '@mui/material';
+import { Drawer, List, ListItemButton, ListItemText, Toolbar, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+
+const drawerWidth = 200;
 
 const Sidebar = () => {
   return (
-    <Drawer variant="permanent" anchor="left">
-      <List>
-        <ListItem button component={Link} to="/">
-          <ListItemText primary="Home" />
-        </ListItem>
-        <ListItem button component={Link} to="/eda">
-          <ListItemText primary="EDA" />
-        </ListItem>
-        {/* Add more sections here later */}
-      </List>
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+        <List>
+          <ListItemButton component={Link} to="/">
+            <ListItemText primary="Home" />
+          </ListItemButton>
+          <ListItemButton component={Link} to="/eda">
+            <ListItemText primary="EDA" />
+          </ListItemButton>
+        </List>
+      </Box>
     </Drawer>
   );
 };
