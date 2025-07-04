1c8731995d717803b2625f5a880f100998fe05c7
Sarah <sarah.meyer@students.fhnw.ch>
2025-07-04 09:14:35 +0200

    cleanded up main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,22 +11,4 @@ app.register_blueprint(umap_api)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
-
-
-# from flask import Flask, jsonify
-# from flask_cors import CORS
-
-# def create_app():
-#     app = Flask(__name__)
-#     CORS(app)
-
-#     @app.route('/api/hello')
-#     def hello():
-#         return jsonify({'message': 'Hello from Flask backend!'})
-
-#     return app
-
-# if __name__ == '__main__':
-#     app = create_app()
-#     app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
