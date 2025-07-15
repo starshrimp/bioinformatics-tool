68337209df89910791a796386586ed60994c2bc9
Sarah <icaze@bluewin.ch>
2025-07-15 13:38:17 +0200

    removed eda (depr)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box, CssBaseline, Paper } from '@mui/material';
 import Sidebar from './components/Sidebar';
 import Landing from './pages/Landing';
-import EDA from './pages/EDA';
 import UMAP from './pages/UMAP';
 import LLMEDA from './pages/LLMEDA';
 import DifferentialExpression from './pages/DifferentialExpression';
@@ -43,7 +42,6 @@ function App() {
           <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
             <Routes>
               <Route path="/" element={<Landing />} />
-              <Route path="/eda" element={<EDA />} />
               <Route path="/differentialexpression" element={<DifferentialExpression />} />
               <Route path="/correlationexplorer" element={<CorrelationExplorer />} />
               <Route path="/llmeda" element={<LLMEDA />} />
